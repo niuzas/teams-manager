5e997484bf1ef8c0e33b4bb96c8b7e4e6bf9cbe7
dariusra2 <darius@nite.lt>
2021-07-08 16:50:45 +0300

    feat: server boilerplate activated

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,21 +1,34 @@
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const router = require('./router/index')
+const errorMiddleware = require('./middlewares/error-middleware');
 
-const app = express();
+const PORT = process.env.PORT || 5000;
+const app = express()
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router);
+app.use(errorMiddleware);
 
-app.get("/", (req, res) => {
-  res.send("OK");
-});
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB Connected...');
+        app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-app.all("*", (req, res) => {
-  res.status(404).send({ error: "Page not found" });
-});
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+start()
