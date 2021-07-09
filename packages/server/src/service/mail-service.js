8174108a8b65936a278923d6bb907d7e47ceeacb
dariusra2 <darius@nite.lt>
2021-07-09 08:17:11 +0300

    bugfix: fixed activation email bug

--- packages/server/src/service/mail-service.js
+++ packages/server/src/service/mail-service.js
@@ -1,34 +1,33 @@
 const nodemailer = require('nodemailer');
 
 class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
 
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false,
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASSWORD
-            }
-        })
-    }
-
-    async sendActivationMail(to, link) {
-        await this.transporter.sendMail({
-            from: process.env.SMTP_USER,
-            to,
-            subject: 'Vartotojo aktyvacija ' + process.env.API_URL,
-            text: '',
-            html:
-                `
+  async sendActivationMail(name, to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: 'Vartotojo aktyvacija ' + process.env.API_URL,
+      text: '',
+      html: `
                     <div>
-                        <h1>Norėdami aktyvuoti vartotoją, paspauskite nuorodą:</h1>
-                        <a href="${link}">${link}</a>
+                     <h2>Sveiki, ${name}! Malonu kad registruojatės.</h2>
+                     <h2>Norėdami aktyvuoti savo vartotoją, paspauskite šią nuorodą:</h2>
+                     <a href="${link}">${link}</a>
                     </div>
-                `
-        })
-    }
+                `,
+    });
+  }
 }
 
 module.exports = new MailService();
