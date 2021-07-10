ec878f624039c887fe50ab4bb77afc21e69300d2
dariusra2 <darius@nite.lt>
2021-07-10 09:45:02 +0300

    feat: dashboard

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,36 +1,16 @@
 import { FC, useContext, useEffect } from 'react';
-import axios from 'axios';
 import { observer } from 'mobx-react-lite';
 
-import LoginForm from './components/LoginForm/LoginForm';
-import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import UsersList from './components/UsersList/UsersList';
 import Loader from './components/Loader/Loader';
 import { Context } from './index';
-import { AuthResponse } from './models/AuthResponse';
-
-import { API_AUTH } from './api';
+import * as P from './pages';
 
 const App: FC = () => {
   const { store } = useContext(Context);
 
   useEffect(() => {
-    async function checkAuth() {
-      store.setLoading(true);
-      try {
-        const response = await axios.get<AuthResponse>(`${API_AUTH}/refresh`, { withCredentials: true });
-        console.log(response);
-        localStorage.setItem('token', response.data.accessToken);
-        store.setAuth(true);
-        store.setUser(response.data.user);
-      } catch (e) {
-        console.log(e.response?.data?.message);
-      } finally {
-        store.setLoading(false);
-      }
-    }
     if (localStorage.getItem('token')) {
-      checkAuth();
+      store.checkAuth();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -42,22 +22,12 @@ const App: FC = () => {
   if (!store.isAuth) {
     return (
       <div>
-        <LoginForm />
-        <RegistrationForm />
+        <P.Home></P.Home>
       </div>
     );
   }
 
-  return (
-    <div>
-      <h1>{store.isAuth ? `Vartotojas autorizuotas ${store.user.email}` : 'AUTORIZUOKITĖS'}</h1>
-      <h1>{store.user.isActivated ? 'Vartotojas patvirtintas (emeilas)' : 'PATVIRTINKITE SAVO EMEILĄ!!!!'}</h1>
-
-      <UsersList></UsersList>
-
-      <button onClick={() => store.logout()}>Atsijungti</button>
-    </div>
-  );
+  return <P.Dashboard></P.Dashboard>;
 };
 
 export default observer(App);
---
+++ packages/client/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,26 @@
+import { FC, useContext } from 'react';
+// import { Context } from '../../index';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../../index';
+import UsersList from '../../components/UsersList/UsersList';
+
+import * as S from './Dashboard.style';
+
+const Dashboard: FC = () => {
+  const { store } = useContext(Context);
+  return (
+    <S.DashboardSection>
+      <h1>Dashboard</h1>
+      <div>
+        <h1>{store.isAuth ? `Vartotojas autorizuotas ${store.user.email}` : 'AUTORIZUOKITĖS'}</h1>
+        <h1>{store.user.isActivated ? 'Vartotojas patvirtintas (emeilas)' : 'PATVIRTINKITE SAVO EMEILĄ!!!!'}</h1>
+
+        <UsersList></UsersList>
+
+        <button onClick={() => store.logout()}>Atsijungti</button>
+      </div>
+    </S.DashboardSection>
+  );
+};
+
+export default observer(Dashboard);
